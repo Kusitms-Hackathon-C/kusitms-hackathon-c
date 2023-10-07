c14dfb0a0f2f2b652c2ae10b3360e2af7cf5f31e
arinming <jyandysys1@naver.com>
2023-10-07 16:31:03 +0900

    [feat] : 네이버 로그인 화면 호출 (#1)

--- app/src/main/java/com/pcandriod/kusitms_hackathon_c/presentation/ui/login/LoginActivity.kt
+++ app/src/main/java/com/pcandriod/kusitms_hackathon_c/presentation/ui/login/LoginActivity.kt
@@ -1,13 +1,22 @@
 package com.pcandriod.kusitms_hackathon_c.presentation.ui.login
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.navercorp.nid.NaverIdLoginSDK
+import com.navercorp.nid.oauth.NidOAuthLogin
+import com.navercorp.nid.oauth.OAuthLoginCallback
+import com.navercorp.nid.profile.NidProfileCallback
+import com.navercorp.nid.profile.data.NidProfileResponse
+import com.pcandriod.kusitms_hackathon_c.BuildConfig
 import com.pcandriod.kusitms_hackathon_c.R
 import com.pcandriod.kusitms_hackathon_c.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private val TAG = this.javaClass.simpleName
     private lateinit var binding: ActivityLoginBinding
 
+    private var name: String = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -15,6 +24,44 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        naverLogin()
+
     }
 
+    private fun naverLogin() {
+        val NAVER_CLIENT_ID = BuildConfig.NAVER_CLIENT_ID
+        val NAVER_CLIENT_SECRET = BuildConfig.NAVER_CLIENT_SECRET
+
+        binding.run {
+            btnNaverLogin.setOnClickListener {
+                val oAuthLoginCallback = object : OAuthLoginCallback {
+                    override fun onSuccess() {
+                        NidOAuthLogin().callProfileApi(object : NidProfileCallback<NidProfileResponse> {
+                            override fun onSuccess(result: NidProfileResponse) {
+                                name = result.profile?.name.toString()
+                                Log.e(TAG, "네이버 로그인 유저 정보 : $name")
+                            }
+
+                            override fun onError(errorCode: Int, message: String) {
+                            }
+
+                            override fun onFailure(httpStatus: Int, message: String) {
+                            }
+                        })
+                    }
+
+                    override fun onError(errorCode: Int, message: String) {
+                        val naverAccessToken = NaverIdLoginSDK.getAccessToken()
+                        Log.e(TAG, "naverAccessToken : $naverAccessToken")
+                    }
+
+                    override fun onFailure(httpStatus: Int, message: String) {
+                    }
+                }
+                NaverIdLoginSDK.initialize(this@LoginActivity, NAVER_CLIENT_ID, NAVER_CLIENT_SECRET, "세이브로드")
+                NaverIdLoginSDK.authenticate(this@LoginActivity, oAuthLoginCallback)
+            }
+        }
+    }
 }
