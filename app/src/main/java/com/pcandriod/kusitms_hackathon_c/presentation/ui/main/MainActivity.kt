ee367a3fb302c5dc1c24f6341c8f3710214638eb
arinming <jyandysys1@naver.com>
2023-10-07 11:06:23 +0900

    해커톤 C팀 초기 세팅🍚

---
+++ app/src/main/java/com/pcandriod/kusitms_hackathon_c/presentation/ui/main/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.pcandriod.kusitms_hackathon_c.presentation.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pcandriod.kusitms_hackathon_c.R
+import com.pcandriod.kusitms_hackathon_c.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+}
